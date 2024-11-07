87175d21585ce495c8700a467287e13f406c7530
Satoshi Ono <s23C1035NN@s.chibakoudai.jp>
2024-11-07 12:10:17 +0900

    Add test

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2024 Satoshi Ono
+# SPDX-FileCopyrightTest: 2024 Satoshi Ono
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
@@ -22,5 +22,5 @@ out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-[ "$res" = 0 ] && echo OK
-exit $res
+[ "${res}" = 0 ] && echo OK
+exit "$res"
